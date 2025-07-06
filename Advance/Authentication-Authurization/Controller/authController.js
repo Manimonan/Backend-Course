bfd97d17c10a9ff2ba4ff5091711baa751859b59
manimohan <manimohan2056@gmail.com>
2025-07-06 20:35:19 +0530

    first commit on backend course

--- Advance/Authentication-Authurization/Controller/authController.js
+++ Advance/Authentication-Authurization/Controller/authController.js
@@ -1,3 +1,4 @@
+const {check} = require("express-validator")
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     pageTitle: "login",
@@ -20,10 +21,57 @@ exports.getSignup =(req,res,next)=>{
     isLoggedIn:false,
   })
 }
-exports.postSignUp =(req,res,next)=>{
+exports.postSignUp =[
+  
+  check("name")
+  .trim()
+  .isLength({min:5})
+  .withMessage("Name should contain atleast 5 charector")
+  .matches(/^[A-Za-z\s]+$/)
+  .withMessage("Name should contain only alphabets"),
+
+  check("email")
+  .isEmail()
+  .withMessage("Please enter a valid email")
+  .normalizeEmail(),
+
+  check("password")
+  .isLength({min:5})
+  .withMessage("Password contain atleast 5 charector")
+  .matches(/[A-Za-z]/)
+  .withMessage("Password contain atlest one uppercase & one lowercase")
+  .matches(/[0-9]/)
+  .withMessage("Password contain atlest on number")
+  .trim(),
+  
+  check("confPassword")
+  .custom((value,{req})=>{
+    if (value !== req.body.password) {
+      throw new Error("Password do not match")
+    }
+    return true;
+  }),
+
+  check("userType")
+  .notEmpty()
+  .withMessage("Please select a user type")
+  .isIn(['guest','host'])
+  .withMessage("Invalid user type"),
+
+  check("terms")
+  .notEmpty()
+  .withMessage("Please accept terms & conditions ")
+  .custom((value,{req})=>{
+    if (value !== "on") {
+      throw new Error("Please accept terms & conditions")
+    }
+    return true;
+  }),
+  
+  (req,res,next)=>{
   console.log(req.body);
   res.redirect("/login")
-}
+}]
 
 exports.postLogout =(req,res,next)=>{
   //res.cookie('isLoggedIn',false)
